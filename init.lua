fbbc5c0e566905941c049c098766ac6f646b4124
ElCeejoYT <40281901+ElCeejoYT@users.noreply.github.com>
2019-05-10 10:46:56 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -36,4 +36,4 @@ dofile(path .. "/sarcosuchus.lua")
 dofile(path .. "/pteranodon.lua")
 
 
-minetest.log("action", "[MOD] PaleoTest v1.21 loaded")
+minetest.log("action", "[MOD] PaleoTest v1.30 loaded")
